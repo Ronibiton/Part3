1dee6c9208076b220df5a1c3b15885dfcee462f6
Roni Biton <ronibiton05@gmail.com>
2024-05-12 15:53:12 +0300

    added first commit oop

---
+++ OOP/OOP/LinkedList.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class LinkedList
+    {
+        private Node _first { get; set; }
+        private Node _last { get; set; }
+        private int _minNode { get; set; }
+        private int _maxNode { get; set; }
+
+
+        public LinkedList(Node first)
+        {
+            _first = first;
+            _last = first;
+            _minNode = first.Value;
+            _maxNode = first.Value;
+        }
+
+        //public bool hasNext()
+        //{
+        //    return _first.Next != null;
+
+        //}
+
+        //public void Append(Node newNode)
+        //{
+        //    Node position = _first;
+        //    while(position.Next!=null)
+        //        position = position.Next;
+        //    position.Next = newNode;
+        //}
+
+        public void Append(Node newNode)
+        {
+            Node position = _last;
+            _last = newNode;
+            position.Next = _last;
+            _minNode = Math.Min(newNode.Value,_minNode);
+            _maxNode = Math.Max(newNode.Value, _maxNode);
+        }
+
+        public void Prepend(Node newNode)
+        {
+            Node position = _first;
+            _first = newNode;
+            _first.Next = position;
+            _minNode = Math.Min(newNode.Value, _minNode);
+            _maxNode = Math.Max(newNode.Value, _maxNode);
+        }
+
+        public int Pop()
+        {
+            Node position = _first;
+            while (position.Next.Next != null)
+                position = position.Next;
+            int value = position.Next.Value;
+            position.Next = position.Next.Next;
+            //if(_maxNode == value)
+            //{
+            //    Sort();
+            //    position = _first;
+            //    while (position.Next!=null)
+            //    {
+            //        _maxNode = position.Value;
+            //        Console.WriteLine(_maxNode);
+            //    }
+            //}
+            //if (_minNode == value)
+            //{
+            //    Sort();
+            //    _minNode = _first.Value;
+            //    Console.WriteLine(_minNode);
+            //}
+
+            return value;
+        }
+        
+        public int Unqueue()
+        {
+            int value = _first.Value;
+            _first = _first.Next;
+            return value;
+        }
+
+
+        //public IEnumerable<int> ToList()
+        //{
+        //    Node position = _first;
+        //    while (position != null)
+        //    {
+        //        yield return position.Value;
+        //        position = position.Next;
+        //    }
+        //    yield break;
+        //}
+
+
+        public IEnumerable<int> ToList()
+        {
+            Node position = _first;
+            List<int> values = new List<int>();
+            while (position != null)
+            {
+                values.Add(position.Value);
+                position = position.Next;
+            }
+            return values;
+        }
+
+
+
+        public bool IsCircular()
+        {
+            Node position = _first.Next;
+            while (position!=null)
+            {
+                if (position == _first)
+                    return true;
+                position = position.Next;
+            }
+            return false;
+        }
+
+        public void Remove( Node preNode,Node node) 
+        {
+            preNode.Next = node;
+        }
+
+        public void Sort()
+        {
+            IEnumerable<int> values = ToList();
+            int[] arr = new int[values.Count()];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = values.ElementAt(i);
+            }
+            Array.Sort(arr);
+            Node position = _first.Next;
+            Node prePosition = _first;
+            foreach(int num in arr)
+            {
+                if (prePosition.Value == num)
+                {
+                    Node node = prePosition;
+                    _first = _first.Next;
+                    node.Next = null;
+                    Append(node);
+                }
+                else
+                {
+                    while (position != null && prePosition != null)
+                    {
+                        if (position.Value == num)
+                        {
+                            Node node = position;
+                            Remove(prePosition, position.Next);
+                            node.Next = null;
+                            Append(node);
+                            break;
+                        }
+                        position = position.Next;
+                        prePosition = prePosition.Next;
+                    }
+                }
+                position = _first.Next;
+                prePosition = _first;
+            }
+        }
+
+
+
+        public void Print()
+        {
+            Node position = _first;
+            while (position != null && position.Next != _first)
+            {
+                Console.WriteLine(position.Value);
+                position = position.Next;
+            }
+            if(position!=null)
+                Console.WriteLine(position.Value);
+        }
+
+        //public void print()
+        //{
+        //    Node position = _first;
+        //    while(position !=null)
+        //    {
+        //        Console.WriteLine(position.Value);
+        //        position = position.Next;
+        //    }
+        //}
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace OOP
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Node n = new Node(5,null);
+            Node n2 = new Node(2, null);
+            Node n3 = new Node(1, null);
+            Node n4 = new Node(7, null);
+            Node n5 = new Node(12, null);
+            Node n6 = new Node(3, null);
+            Node n7 = new Node(9, null);
+            Node n8 = new Node(11, null);
+            Node n9 = new Node(111, null);
+            //LinkedList<Node> l = new LinkedList<Node>();
+            LinkedList l = new LinkedList(n);
+            Console.WriteLine() ;
+            l.Prepend(n3);
+            l.Append(n2);
+            l.Prepend(n4);
+            l.Append(n5);
+            l.Append(n6);
+            l.Append(n7);
+            l.Prepend(n8);
+            l.Append(n9);
+            l.Print();
+            Console.WriteLine();
+            Console.WriteLine(l.Pop());
+            //l.Append(n4);
+            //Console.WriteLine();
+            //int x = l.Pop();
+            //int y = l.Unqueue();
+            //Console.WriteLine(l.IsCircular());
+            //l.Print();
+            //l.Sort();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            Console.WriteLine();
+            l.Print();
+
+            //Console.WriteLine( l.ToList());
+
+            //foreach (int num in l.ToList())
+            //{
+            //    Console.WriteLine(num) ;
+            //}
+
+            //Node X = l.Sort();
+            //while(X!=null)
+            //{
+            //    Console.WriteLine(X.Value);
+            //}
+
+            //Console.WriteLine("here" + x);
+            //Console.WriteLine("heree" + y);
+
+            //Console.WriteLine(l.ToList());
+        }
+    }
+}
